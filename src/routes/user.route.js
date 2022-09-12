e8893fa4f32763eb84c85eee4091e7fa8052bd9c
Mihir Verma <mihir.verma@itential.com>
2022-09-12 10:30:36 +0530

    added swagger info

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -23,10 +23,45 @@ const userRouter = express.Router();
  *       description: Success Response
  *
  */
-
 userRouter.post("/signup", signup);
+
+/**
+ * @swagger
+ * /api/forgotpassword:
+ *   post:
+ *     summary: Used for generating a token for forgot password
+ *     description: Used for send an reset email request to user.
+ *     responses:
+ *     200:
+ *       description: {Success Response}
+ *
+ */
 userRouter.post("/forgotpassword", forgotPassword);
+
+/**
+ * @swagger
+ * /api/resetPassword:
+ *   put:
+ *     summary: Used for update passord
+ *     description: Used to update the password after forgetting.
+ *     responses:
+ *     200:
+ *       description: {message: "string", token: "jwt token"}
+ *
+ */
 userRouter.put("/resetPassword", resetPassword);
+
+/**
+ * @swagger
+ * /api/uploadProfileImage:
+ *   put:
+ *     summary: Used for update profile image of the user
+ *     description: Used to update the profile image of user.
+ *     responses:
+ *     200:
+ *       description: {message:"string", image_url: "https://abc.png"}
+ *
+ */
 userRouter.put(
   "/uploadProfileImage",
   validateToken,
@@ -34,6 +69,18 @@ userRouter.put(
   multerConfigs.single("profileImage"),
   uploadProfileImage
 );
+
+/**
+ * @swagger
+ * /api/udpateUserInfo:
+ *   put:
+ *     summary: Used for update info of the user
+ *     description: Used to update the info of user.
+ *     responses:
+ *     200:
+ *       description: {message:"string", userObject}
+ *
+ */
 userRouter.put("/udpateUserInfo", validateToken, validateUser, updateUserInfo);
 
 module.exports = userRouter;
